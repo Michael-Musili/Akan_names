bac401ed491e51f553bd78656366222402d817cb
Michael-Musili <93195066+Michael-Musili@users.noreply.github.com>
2021-12-14 02:30:45 +0300

    added content to js

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,24 @@
 
-function AName () {
+function akanName () {
     let dateOfBirth = Number(document.getElementById("year").value);
     let monthOfBirth = Number(document.getElementById("month").value);
     let yearOfBirth = document.getElementById("year").value ;
     let genders = document.getElementsByName("gender");
+
+
+}
+
+var dayNames = ["Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday","Saturday" ];
+var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
+var femaleNames = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
+
   
    
     // month validation 
     function monthValidator () {
       if (monthOfBirth < 1 || monthOfBirth > 12) {
-        return false;
+        alert("invalid month");
+        return  false;
       } else {
         return true;
       }
@@ -17,7 +26,21 @@ function AName () {
      // date validation
      function dateValidator () {
         if (dateOfBirth < 1 || dateOfBirth > 31) {
-          return false;
+          alert ("invalid date");
+            return false;
         } else {
           return true;
         }
+    }
+    //gender validation
+    function validate(form)
+    {
+        var genderM=form.gender_male.value;
+        var genderF=form.gender_female.value;
+    
+        if(genderM.checked==false && genderF.checked==false )
+           {
+                alert("You must select male or female");
+                return false;
+           }   
+     }
