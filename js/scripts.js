c347f06f1ea0b05f43917c79b732ef05eb75d9cf
Michael-Musili <93195066+Michael-Musili@users.noreply.github.com>
2021-12-13 11:14:52 +0300

    added some js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,23 @@
-var dayNames = ["Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday","Saturday" ];
-var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
-var femaleNames = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
 
+function AName () {
+    let dateOfBirth = Number(document.getElementById("year").value);
+    let monthOfBirth = Number(document.getElementById("month").value);
+    let yearOfBirth = document.getElementById("year").value ;
+    let genders = document.getElementsByName("gender");
+  
+   
+    // month validation 
+    function monthValidator () {
+      if (monthOfBirth < 1 || monthOfBirth > 12) {
+        return false;
+      } else {
+        return true;
+      }
+    }
+     // date validation
+     function dateValidator () {
+        if (dateOfBirth < 1 || dateOfBirth > 31) {
+          return false;
+        } else {
+          return true;
+        }
