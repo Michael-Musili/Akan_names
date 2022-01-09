9c1b527ec3ca1bd01e110e5cfd33c63021f6bd30
Michael-Musili <93195066+Michael-Musili@users.noreply.github.com>
2022-01-09 19:32:53 +0300

    added Javascript.

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,46 @@
-
-function akanName () {
-    let dateOfBirth = Number(document.getElementById ("year").value);
-    let monthOfBirth = Number(document.getElementById("month").value);
-    let yearOfBirth = document.getElementById("year").value ;
-    let genders = document.getElementsByName("gender");
-}
-
-var dayNames = ["Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday","Saturday" ];
-var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
+var maleNames= ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
 var femaleNames = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
+var weekday =["Monday","Tuesday","Wednesday","Thursday","Friday"];
+
+
+
+function generateAkan() {
+  var year = document.getElementById("year").value;
+  var month = document.getElementById("month").value;
+  var day = document.getElementById("day").value;
+  var male = document.getElementById ("male").value;
+  var female = document.getElementById ("female").value;
+  var century =parseInt((year.slice(0,2)));
+  
 
   
-   
-    // month validation 
-    function monthValidator () {
-      if (monthOfBirth < 1 || monthOfBirth > 12) {
-        alert("invalid month");
-        
-      } else {
-        alert("true")
-        
-        dateOfBirth < 1 || dateOfBirth > 31) {
-          alert ("invalid date");
-            
-        } else {
-          alert("true")
-        }
-    };
-    //gender validation
-    function validate(form)
-    {
-        var genderM=form.gender_male.value;
-        var genderF=form.gender_female.value;
-    
-        if(genderM.checked==false && genderF.checked==false )
-           {
-                alert("You must select male or female");
-                return false;
-           }   
-     }
+  //let dayOfWeek=((((century/ 4)-2 *century-1)+((5*year/4))+((26*(month+1)/10))+day)%7);
+ //let dayOfWeek=(( ( (century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + day)%7);
+ //var dayOfWeek = parseInt((((century / 4) - 2 * century - 1) + ((5 * year / 4)) + ((26 * (month + 1) / 10)) + day) % 7)-5;
+ //var dayOfWeek = parseInt(((cc / 4) - 2 * cc - 1) + ((5 * yy / 4)) + ((26 * (mm + 1) / 10)) + dd) % 7;
+//console.log(century)
+
+let birthday = new Date(`${year}-${month}-${day}`)
+  let dateString = birthday.toDateString().slice(0,3)
+  let birthDate = birthday.getDay()
+  console.log(dateString)
+
+
+if (male == "male"){
+  let name = maleNames[birthDate]
+  alert ("You were born on a " +  dateString + "  So your  Akan Name will be " + name)
+}
+else if (female == "female"){
+  let name = femaleNames[birthDate]
+  alert ("You were born on a " +  dateString + "  So Your Akan Name will be is" + name)
+}
+else {
+  alert("Please select gender")
+}
+if (month > 12 || month < 1){
+   alert ("Invalid month")
+}
+if (day > 31 || month <1){
+  alert ("Invalid date")
+}
+};
